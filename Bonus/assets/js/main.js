9ad58504cf8e93a31583c387a57b920ce5a0407d
SimoneLollini <lollinisimone@gmail.com>
2022-10-21 10:03:55 +0200

    fix variables name add regions

--- Bonus/assets/js/main.js
+++ Bonus/assets/js/main.js
@@ -1,10 +1,10 @@
+//#region instruction
 // Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 // MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
 // MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
 // BONUS 1: Trasformare la stringa foto in una immagine effettiva
 // BONUS 2: Organizzare i singoli membri in card/schede
-
 // Name: Wayne Barnett
 // Role: Founder & CEO
 // Image: wayne-barnett-founder-ceo.jpg
@@ -28,11 +28,11 @@
 // Name: Barbara Ramos
 // Role: Grafic designer
 // Image: barbara-ramos-graphic-designer.jpg
-
+//#endregion instruction
 const rowElement = document.querySelector('.row')
 
 // MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
-
+//#region team
 team = [
     {
         name: 'Wayne Barnett',
@@ -62,31 +62,19 @@ team = [
 ]
 console.log(team);
 
+//#endregion team
 
 // MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
 
 for (let i = 0; i < team.length; i++) {
-    const person = team[i];
-    // console.log(person);
-    for (const key in person) {
-        const personKeys = person[key];
+    const member = team[i];
+    // console.log(member);
+    for (const key in member) {
+        const personKeys = member[key];
         // console.log(personKeys);
     }
     // MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
-    // const colEl = document.createElement('div')
-    // const h3El = document.createElement('h3')
-    // const h6El = document.createElement('h6')
-    // const imgEl = document.createElement('img')
-    // colEl.classList.add('col')
-    // rowElement.append(colEl)
-    // colEl.append(h3El, h6El, imgEl)
-    // h3El.append(person.name)
-    // h6El.append(person.role)
-    // imgEl.setAttribute("src", `./assets/img/${person.img}`);
-    generateMarkup(person)
-}
-
-function generateMarkup(person) {
+    //#region no function method 
     const colEl = document.createElement('div')
     const h3El = document.createElement('h3')
     const h6El = document.createElement('h6')
@@ -94,9 +82,24 @@ function generateMarkup(person) {
     colEl.classList.add('col')
     rowElement.append(colEl)
     colEl.append(h3El, h6El, imgEl)
-    h3El.append(person.name)
-    h6El.append(person.role)
-    imgEl.setAttribute("src", `./assets/img/${person.img}`);
+    h3El.append(member.name)
+    h6El.append(member.role)
+    imgEl.setAttribute("src", `./assets/img/${member.img}`);
+    //#endregion
+    generateMarkup(member)
+}
+
+function generateMarkup(member) {
+    const colEl = document.createElement('div')
+    const h3El = document.createElement('h3')
+    const h6El = document.createElement('h6')
+    const imgEl = document.createElement('img')
+    colEl.classList.add('col')
+    rowElement.append(colEl)
+    colEl.append(h3El, h6El, imgEl)
+    h3El.append(member.name)
+    h6El.append(member.role)
+    imgEl.setAttribute("src", `./assets/img/${member.img}`);
 }
 
 
